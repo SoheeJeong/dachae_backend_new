0551820f753d057cff92ea25618ed70ce0faf282
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-01-06 21:29:24 +0900

    add urls of common and matching

--- dachae/urls.py
+++ dachae/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dachae/common/',include('common.urls',namespace='common')),
+    path('dachae/matching/',include('matching.urls',namespace='macthing'))
 ]
