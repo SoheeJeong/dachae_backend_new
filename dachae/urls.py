c57cc89070e1dc78c8caba755570831f589b25cf
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-01-07 00:38:56 +0900

    feature: add swagger settings

--- dachae/urls.py
+++ dachae/urls.py
@@ -1,30 +1,35 @@
-"""dachae URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
 from django.conf import settings
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 import common.urls
 import matching.urls
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Dachae API",
+      default_version='v1',
+      description="엑셀 api 문서 링크: https://docs.google.com/spreadsheets/d/1IspLKL2Z2obXS_n2nJ3AK0zWLfWgUYGQnupBsiZTxJA/edit#gid=0",
+      terms_of_service="https://www.google.com/policies/terms/", #TODO change
+      contact=openapi.Contact(email="jshgooacc@gmail.com"),
+      license=openapi.License(name="BSD License"), #TODO change
+   ),
+   public=False,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/',schema_view.with_ui(cache_timeout=0),name='swagger'),
     path('dachae/common/',include(common.urls,namespace='common')),
     path('dachae/matching/',include(matching.urls,namespace='macthing'))
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
