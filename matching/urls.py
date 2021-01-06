7516e630ae70e0de561dce3055600b474e084bb0
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-01-06 22:44:54 +0900

    feature: add view functions and set urls of matching

--- matching/urls.py
+++ matching/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name = 'matching'
 urlpatterns = [
+    path('getPictureFilteredResult/', views.get_picture_filtered_result, name='getPictureFilteredResult'),
     path('getPictureDetailInfo/', views.get_picture_detail_info, name='getInferenceSetting'),
+    path('getLabelList/', views.get_label_list, name='getLabelList'),
+    path('setUserImageUpload/', views.set_user_image_upload, name='setUserImageUpload'),
+    path('execRecommend/', views.exec_recommend, name='execRecommend'),
+    path('getRecommendResult/', views.get_recommend_result, name='getRecommendResult'),
 ]
--- matching/views.py
+++ matching/views.py
@@ -16,9 +16,55 @@ import datetime
 import random
 import base64
 
-from dachae import models
+from dachae.models import TbArkworkInfo,TbCompanyInfo,TbLabelInfo,TbUploadInfo,TbUserInfo,TbUserLog,TbWishlistInfo
+from dachae.exceptions import DataBaseException
+
+@api_view(["GET"])
+def get_picture_filtered_result(request):
+    '''
+    키워드 및 느낌라벨로 명화 검색
+    '''
+    data = {"data":"temp"}
+    return Response(data)
 
 @api_view(["GET"])
 def get_picture_detail_info(request):
+    '''
+    명화 상세정보 가져오기
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@api_view(["GET"])
+def get_label_list(request):
+    '''
+    느낌라벨 리스트 가져오기
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@csrf_exempt
+@api_view(["POST"])
+def set_user_image_upload(request):
+    '''
+    사용자 로컬이미지 업로드
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@csrf_exempt
+@api_view(["POST"])
+def exec_recommend(request):
+    '''
+    매칭 수행
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@api_view(["GET"])
+def get_recommend_result(request):
+    '''
+    매칭 결과 보여주기
+    '''
     data = {"data":"temp"}
     return Response(data)
