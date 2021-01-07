25fae451fccaaa1d7e3cfab27a218822b38a10ac
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-01-07 12:22:51 +0900

    feature: dachae common views basic settings and descriptions

--- dachae_common/views.py
+++ dachae_common/views.py
@@ -2,7 +2,37 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(["GET"])
+@csrf_exempt
+@api_view(["POST"])
 def set_signup(request):
+    '''
+    회원가입
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@csrf_exempt
+@api_view(["POST"])
+def set_signin(request):
+    '''
+    로그인
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@csrf_exempt
+@api_view(["POST"])
+def set_signout(request):
+    '''
+    로그아웃
+    '''
+    data = {"data":"temp"}
+    return Response(data)
+
+@api_view(["GET"])
+def get_best_image_list(request):
+    '''
+    메인화면에서 샘플사진 리스트 로드
+    '''
     data = {"data":"temp"}
     return Response(data)
