0922469b3e16da7283f1b28e71a03a51834b8617
soheejeong <wjdthgmlg@korea.ac.kr>
2021-01-20 01:42:01 +0900

    cors    주

--- dachae/settings.py
+++ dachae/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'drf_yasg', #swagger
-    'corsheaders',
+    # 'corsheaders',
 
     #apps
     'dachae',
@@ -73,7 +73,7 @@ LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,8 +135,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = False
 
 # CORS_ORIGIN_WHITELIST = [
 #     "https://localhost:3000",
