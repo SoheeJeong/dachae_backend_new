b31aa3777118d21e2c6d6a30f4b8846aa99dc798
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-01-20 13:40:43 +0900

    config and cors setting fixed

--- dachae/settings.py
+++ dachae/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'drf_yasg', #swagger
-    'corsheaders',
+    #'corsheaders',
 
     #apps
     'dachae',
@@ -71,7 +71,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_ALLOW_ALL = True
 #CORS_ALLOW_CREDENTIALS = True
 
 # CORS_ORIGIN_WHITELIST = [
