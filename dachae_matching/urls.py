69e331727e11df8f1e3e9b1e0f0e42b1f30893b7
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-04-06 00:29:41 +0900

    remove urls: unneeded function's urls

--- dachae_matching/urls.py
+++ dachae_matching/urls.py
@@ -3,13 +3,7 @@ from . import views
 
 app_name = 'matching'
 urlpatterns = [
-    path('getBestImageList/', views.get_best_image_list, name='getBestImageList'),
-    path('getPictureFilteredResult/', views.get_picture_filtered_result, name='getPictureFilteredResult'),
     path('getPictureDetailInfo/', views.get_picture_detail_info, name='getInferenceSetting'),
-    path('getLabelList/', views.get_label_list, name='getLabelList'),
     path('setUserImageUpload/', views.set_user_image_upload, name='setUserImageUpload'),
     path('execRecommend/', views.exec_recommend, name='execRecommend'),
-    path('setWishList/', views.set_wish_list, name='setWishList'),
-    path('delWishList/', views.del_wish_list, name='delWishList'),
-    path('loadPurchaseLink/', views.load_purchase_link, name='loadPurchaseLink'),
 ]
