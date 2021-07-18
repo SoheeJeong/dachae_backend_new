5dfe4235285fe3c421e9bcd1a75a461cd598d763
SoheeJeong <wjdthgmlgo@korea.ac.kr>
2021-07-19 00:58:03 +0900

    feature: combine pic detail info into recommend

--- dachae_matching/urls.py
+++ dachae_matching/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 app_name = 'matching'
 urlpatterns = [
-    path('getPictureDetailInfo/', views.get_picture_detail_info, name='getPictureDetailInfo'),
     path('setUploadAndRecommend/', views.set_upload_and_recommend, name='setUploadAndRecommend'),
     path('getDefaultRecommend/', views.get_default_recommend, name='getDefaultRecommend')
 ]
